a61d2d22b1d6fccd751de9da6fcb21646c2213ee
webDevSaurav <webdevelopersaurav@gmail.com>
2020-08-03 21:14:30 +0530

    added the accordion

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import Accordian from './components/Accordian';
+
+const items = [
+    {
+        title: "What is React?",
+        content: "React is frontend js library"
+    },
+    {
+        title: "How to use React",
+        content: "Use react by creating components"
+    },
+    {
+        title: "Why use React?",
+        content: "React is amazing js library"
+    }
+]
 
 const App = () => {
     return (
         <div>
-            Widget App
+            <Accordian items={items} />
         </div>
     )
 }
